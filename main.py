4019532cf9a5d461fcbd05b6602b743f49b36d87
Darren Conyngham <dconyngham92@gmail.com>
2020-11-25 12:19:16 +0200

    moved card list inside function

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from random import choice
 from art import logo
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     return choice(cards)
 
 
